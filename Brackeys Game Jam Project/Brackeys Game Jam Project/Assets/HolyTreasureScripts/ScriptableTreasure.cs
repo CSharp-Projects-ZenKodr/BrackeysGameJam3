aeb1bd4d13a22d0d458bfca6d0d44835b2d4a934
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-02-16 15:41:42 -0500

    Complete Treasure Chest mechanic
    Completed Treasure Chest Mecmanic so that chest opens up when player runs into it, then a window will display what was in it, and then the player can get back to playing.   Other more specific functions were also added in the process.

---
+++ Brackeys Game Jam Project/Brackeys Game Jam Project/Assets/HolyTreasureScripts/ScriptableTreasure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.HolyTreasureScripts {
+    [CreateAssetMenu(fileName ="New Treasure", menuName = "Treasure")]
+    public class ScriptableTreasure : ScriptableObject {
+        #region MyRegion
+        /// <summary>
+        /// The Icon of the Treasure.
+        /// </summary>
+        public Sprite treasureIcon;
+        /// <summary>
+        /// The Name of the Treasure.
+        /// </summary>
+        public string treasureName;
+        /// <summary>
+        /// The Description of the Treasure.
+        /// </summary>
+        [TextArea]
+        public string treasureDescription;
+        /// <summary>
+        /// The Value of the Treasure.
+        /// </summary>
+        public int treasureValue;
+        #endregion
+    }
+}
--- Brackeys Game Jam Project/Brackeys Game Jam Project/Assets/HolyTreasureScripts/TreasureChest.cs
+++ Brackeys Game Jam Project/Brackeys Game Jam Project/Assets/HolyTreasureScripts/TreasureChest.cs
@@ -3,25 +3,102 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using Assets.HolyTreasureScripts.UI;
 
 namespace Assets.HolyTreasureScripts {
     public class TreasureChest : MonoBehaviour {
 
         #region Variables
         /// <summary>
+        /// The Treasure this chest holds.
+        /// </summary>
+        public ScriptableTreasure treasure;
+        /// <summary>
+        /// The gradient the wood will tint to.
+        /// </summary>
+        public Gradient woodGradient;
+        /// <summary>
+        /// The evaluation of the gradient.
+        /// </summary>
+        [Range(0, 1)]
+        public float evaluation = 0;
+        /// <summary>
+        /// The rate at which the treasure chest will decay into grey.
+        /// </summary>
+        public float decayRate = 0.1f;
+
+        /// <summary>
         /// The Animator attached to this treasure chest.
         /// </summary>
         private Animator attachedAnimator;
+        /// <summary>
+        /// The Renderer attached to this treasure chest.
+        /// </summary>
+        private Renderer attachedRenderer;
+        /// <summary>
+        /// The Renderer attached to this treasure chest's lid.
+        /// </summary>
+        private Renderer lidRenderer;
+        /// <summary>
+        /// The material that represents red wood on the chest.
+        /// </summary>
+        private Material chestRedWoodMaterial;
+        /// <summary>
+        /// The material that represents red wood on the lid.
+        /// </summary>
+        private Material lidRedWoodMaterial;
+        /// <summary>
+        /// Return true if player got the treasure, or false if not.
+        /// </summary>
+        private bool treasureGot = false;
         #endregion
 
         private void Start() {
             attachedAnimator = GetComponent<Animator>();
+            attachedRenderer = GetComponent<Renderer>();
+            lidRenderer = transform.GetChild(0).GetComponent<Renderer>();
+            chestRedWoodMaterial = attachedRenderer.materials.Last();
+            lidRedWoodMaterial = lidRenderer.materials.Last();
+
+            //TODO: Comment this back in for saving
+            //string varName = "Key_" + treasure.treasureName + "_Status";
+            //int status = PlayerPrefs.GetInt(varName);
+            //if (status == 0) {
+            //    treasureGot = false;
+            //} else if (status == 1) {
+            //    treasureGot = true;
+            //    chestRedWoodMaterial.color = woodGradient.Evaluate(1);
+            //    lidRedWoodMaterial.color = woodGradient.Evaluate(1);
+            //    evaluation = 1;
+            //} else {
+            //    Debug.LogError("Failed to get a proper status for a treasure chest!", gameObject);
+            //}
+        }
+
+        private void Update() {
+            if (treasureGot) {
+                if (evaluation < 1) {
+                    evaluation += decayRate * Time.deltaTime;
+                    chestRedWoodMaterial.color = woodGradient.Evaluate(evaluation);
+                    lidRedWoodMaterial.color = woodGradient.Evaluate(evaluation); 
+                }
+            }
+
         }
 
         private void OnTriggerEnter(Collider other) {
-            if (other.tag == "Player") {
-                Debug.Log("Open Chest");
-                attachedAnimator.SetTrigger("Open");
+            if (!treasureGot) {
+                if (other.tag == "Player") {
+                    ThirdPersonUserControl useCon = other.GetComponent<ThirdPersonUserControl>();
+                    useCon.ableToMove = false;
+
+                    attachedAnimator.SetTrigger("Open");
+                    TreasureNotification.Instance.PopUpCard(treasure);
+
+                    treasureGot = true;
+                    GlobalConfig.SaveTreasureChestStatus(treasure.treasureName, treasureGot);
+                } 
             }
         }
     }
