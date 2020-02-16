76ccb616dd6460026cc82958053c9b66fffad2fe
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-02-16 16:23:57 -0500

    Added Player Inventory and working on Pause Screen
    Added a class that will hold the player's inventory, which will be displayed on the pause screen and used elsewhere.

---
+++ Brackeys Game Jam Project/Brackeys Game Jam Project/Assets/HolyTreasureScripts/PlayerInventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.HolyTreasureScripts {
+    public class PlayerInventory : MonoBehaviour {
+
+        #region MyRegion
+        /// <summary>
+        /// The instance of this class.
+        /// </summary>
+        public static PlayerInventory Instance;
+
+        /// <summary>
+        /// The money the player currently has on hand.
+        /// </summary>
+        public int currentMoney = 0;
+
+        /// <summary>
+        /// The list of treasures the player has aquired.
+        /// </summary>
+        private List<ScriptableTreasure> acquiredTreasures;
+        #endregion
+
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            } else {
+                Debug.LogError("There is more than one Player Inven class in the scene!");
+            }
+        }
+    }
+}
