e537cd49e7392e25629d7188d43dfd93d41587b6
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-02-16 12:33:42 -0500

    Adding Third Person Controller and Treasure Chest

---
+++ Brackeys Game Jam Project/Brackeys Game Jam Project/Assets/HolyTreasureScripts/TreasureChest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.HolyTreasureScripts {
+    public class TreasureChest : MonoBehaviour {
+
+        #region Variables
+        /// <summary>
+        /// The Animator attached to this treasure chest.
+        /// </summary>
+        private Animator attachedAnimator;
+        #endregion
+
+        private void Start() {
+            attachedAnimator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.tag == "Player") {
+                Debug.Log("Open Chest");
+                attachedAnimator.SetTrigger("Open");
+            }
+        }
+    }
+}
---
+++ Brackeys Game Jam Project/Holes.mm
@@ -0,0 +1,79 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1581863225503" ID="ID_1807140299" MODIFIED="1581863662082" TEXT="Holes">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1581863313956" ID="ID_471496993" MODIFIED="1581863649045" POSITION="right" TEXT="Not Fun">
+<icon BUILTIN="folder"/>
+<node CREATED="1581863887271" ID="ID_544333349" MODIFIED="1581863891936" TEXT="Creates a gap"/>
+<node CREATED="1581863893811" ID="ID_51643004" MODIFIED="1581863905709" TEXT="Can accidently be stepped in"/>
+<node CREATED="1581864112470" ID="ID_1832377334" MODIFIED="1581864125785" TEXT="Can uncover something that otherwise should remain hidden"/>
+<node CREATED="1581864388706" ID="ID_1064882928" MODIFIED="1581864398242" TEXT="Too large, ruins plans"/>
+<node CREATED="1581864429827" ID="ID_1241305239" MODIFIED="1581864440560" TEXT="Lets things in like Monsters or the cold"/>
+</node>
+<node CREATED="1581863324575" ID="ID_650594146" MODIFIED="1581863649047" POSITION="left" TEXT="Fun">
+<icon BUILTIN="folder"/>
+<node CREATED="1581863405427" ID="ID_668040434" MODIFIED="1581863649047" TEXT="Metaphorical">
+<icon BUILTIN="folder"/>
+<node CREATED="1581863870731" ID="ID_306747279" MODIFIED="1581863878970" TEXT="A relationship to be made"/>
+<node CREATED="1581864364472" ID="ID_1470915366" MODIFIED="1581864373179" TEXT="Something missing that can be replaced"/>
+</node>
+<node CREATED="1581863392737" ID="ID_1230743409" MODIFIED="1581864567285" TEXT="Physical">
+<icon BUILTIN="folder"/>
+<node CREATED="1581863415646" ID="ID_288492673" MODIFIED="1581863641798" TEXT="Making">
+<icon BUILTIN="folder"/>
+<node CREATED="1581863724694" ID="ID_1600748104" MODIFIED="1581863745762" TEXT="Making it to realize a bigger purpose"/>
+<node CREATED="1581863720420" ID="ID_64927700" MODIFIED="1581863724202" TEXT="Digging it"/>
+<node CREATED="1581863753526" ID="ID_852507370" MODIFIED="1581863864852" TEXT="Gaining Materials from the hole"/>
+<node CREATED="1581864338611" ID="ID_672665751" MODIFIED="1581864342256" TEXT="Destruction"/>
+<node CREATED="1581864536438" ID="ID_788177703" MODIFIED="1581864543684" TEXT="To explore the new space made"/>
+</node>
+<node CREATED="1581863420570" ID="ID_567753361" MODIFIED="1581863641798" TEXT="Filling">
+<icon BUILTIN="folder"/>
+<node CREATED="1581863701822" ID="ID_1003458993" MODIFIED="1581863717492" TEXT="Sewing or patching it back together"/>
+<node CREATED="1581863690732" ID="ID_1272812612" MODIFIED="1581863697013" TEXT="Filling a gap"/>
+<node CREATED="1581863678082" ID="ID_473036403" MODIFIED="1581863689422" TEXT="Covering it up"/>
+<node CREATED="1581863667384" ID="ID_1056891899" MODIFIED="1581863677683" TEXT="Putting something in it"/>
+<node CREATED="1581864324174" ID="ID_1485013063" MODIFIED="1581864335832" TEXT="Making the ground the hole is in level again"/>
+<node CREATED="1581864495461" ID="ID_1364570476" MODIFIED="1581864506454" TEXT="Pushing/Fitting Something into it"/>
+</node>
+<node CREATED="1581863619834" ID="ID_1715290220" MODIFIED="1581863630355" TEXT="Their irregular shape"/>
+<node CREATED="1581863845339" ID="ID_970343703" MODIFIED="1581864306124" TEXT="Taking something out of it i.e. Operation"/>
+<node CREATED="1581863915612" ID="ID_993192990" MODIFIED="1581863924260" TEXT="Exploring what&apos;s inside"/>
+</node>
+</node>
+<node CREATED="1581863956953" ID="ID_1505320857" MODIFIED="1581863964945" POSITION="right" TEXT="The Idea">
+<icon BUILTIN="idea"/>
+<node CREATED="1581864130736" ID="ID_780425598" MODIFIED="1581864724805" TEXT="Idea 1">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1581863968373" ID="ID_481102198" MODIFIED="1581863995384" TEXT="Create an update in the Great Cave Offensive from Kirby Super Star"/>
+<node CREATED="1581864000093" ID="ID_1036542289" MODIFIED="1581864013546" TEXT="Collect Treasure to gain money"/>
+<node CREATED="1581864017351" ID="ID_1402219790" MODIFIED="1581864036099" TEXT="Use Money to buy better equipment and upgrades"/>
+<node CREATED="1581864040298" ID="ID_1353538143" MODIFIED="1581864051844" TEXT="Monsters can be found in the cave"/>
+<node CREATED="1581864054132" ID="ID_616778452" MODIFIED="1581864074945" TEXT="You play as a miner that only use Tools to make holes and caves"/>
+<node CREATED="1581864726641" ID="ID_556120882" MODIFIED="1581864798486" TEXT="Why this one">
+<node CREATED="1581864730903" ID="ID_1583180697" MODIFIED="1581864741582" TEXT="Great and Simple for a Game Jam"/>
+<node CREATED="1581864745637" ID="ID_1912579354" MODIFIED="1581864761232" TEXT="Has an established game in its inspiration"/>
+<node CREATED="1581864774650" ID="ID_246308474" MODIFIED="1581864785055" TEXT="Should be easy to program and structure"/>
+</node>
+</node>
+<node CREATED="1581864145194" ID="ID_595118" MODIFIED="1581864722279" TEXT="Idea 2 ">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1581864149878" ID="ID_1994090614" MODIFIED="1581864170740" TEXT="You play as a being that lives in a broken down human making factory"/>
+<node CREATED="1581864173248" ID="ID_1846139387" MODIFIED="1581864198393" TEXT="This being has holes in its body where body parts should be"/>
+<node CREATED="1581864201297" ID="ID_1161224765" MODIFIED="1581864222604" TEXT="You also live with other beings who have holes in their bodies"/>
+<node CREATED="1581864225487" ID="ID_1281402410" MODIFIED="1581864255323" TEXT="You must go around the factory and collect the body parts you need to complete your fellow beings"/>
+<node CREATED="1581864458072" ID="ID_846251112" MODIFIED="1581864484421" TEXT="To get these body parts, you have to complete puzzles that have to do with holes"/>
+</node>
+<node CREATED="1581864593902" ID="ID_199430445" MODIFIED="1581864704842" TEXT="Idea 3">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1581864599977" ID="ID_661677876" MODIFIED="1581864622677" TEXT="You play as a Sewing Man, a person whose sole job is to patch holes"/>
+<node CREATED="1581864629070" ID="ID_1148634585" MODIFIED="1581864643583" TEXT="Holes are ripping in his village&apos;s ground"/>
+<node CREATED="1581864645894" ID="ID_1854803166" MODIFIED="1581864662322" TEXT="You must use a giant sewing needle to fix the ground"/>
+<node CREATED="1581864677223" ID="ID_1015298315" MODIFIED="1581864699902" TEXT="Sew the holes so that the village can go back to normal, and complete their tasks"/>
+</node>
+</node>
+</node>
+</map>
